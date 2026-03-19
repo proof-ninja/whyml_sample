96db3e04c16c4861b318a74305ee99b463098a6f
ymoymoon <148025734+ymoymoon@users.noreply.github.com>
2026-03-19 16:43:45 +0900

    comment added

--- bin/examples.ml
+++ bin/examples.ml
@@ -6,8 +6,15 @@ open Infv.Tools
 (* ----- example 1 ----- 
     assert (forall x, x == x) *)
 (* 量化子をつける *)
-let mk_binder x : binder = (pos, Some (mk_ident x), false, None)
-let example1 : expr = mk_assert (~@ (Tquant (DTforall, [mk_binder "x"], [], eq (mk_Tvar "x") (mk_Tvar "x"))))
+let mk_binder x ty : binder = (pos, 
+  Some (mk_ident x), 
+  true, (* goast かどうか...「実行」されるか，「証明専用」か by ChatGPT *)
+  ty) (* x の型情報 *)
+let example1 : expr = mk_assert (~@ (Tquant (
+  DTforall, 
+  [mk_binder "x" None], 
+  [], (* トリガー？ *)
+  eq (mk_Tvar "x") (mk_Tvar "x"))))
 
 
 (* ----- example 2 ----- 
@@ -15,7 +22,10 @@ let example1 : expr = mk_assert (~@ (Tquant (DTforall, [mk_binder "x"], [], eq (
     if n then assert (n == true)
     else assert (n == false) *)
 
-let example2 = Elet (mk_ident "n", false, RKlocal, ~! Etrue, 
+let example2 = Elet (mk_ident "n", 
+  false, (* goast ではない *)
+  RKlocal, (* local 変数 *)
+  ~! Etrue, (*代入される値 *)
   ~! (Eif (mk_Evar "n", 
     mk_assert (eq (mk_Tvar "n") (~@ Ttrue)),
     mk_assert (eq (mk_Tvar "n") (~@ Tfalse)))))
@@ -35,7 +45,12 @@ let spec3 = {
   sp_diverge = false;
   sp_partial = false
 }
-let example3 : expr = ~! (Efun ([mk_binder "n"], None, mk_Pvar "n", MaskVisible, spec3, 
+let example3 : expr = ~! (Efun (
+  [mk_binder "n" (Some (PTtyvar (mk_ident "i32")))], (* 引数 *)
+  None, (* 関数の型 *)
+  mk_Pvar "res", (* 返り値パターン（タプルとかの場合もある） *)
+  MaskVisible, (* 副作用？ *)
+  spec3, (* 仕様 *)
   ~! (Epure (plus (mk_Tvar "n") (mk_Tvar "n")))))
 
 (* ----- example 4 -----
@@ -43,3 +58,37 @@ let example3 : expr = ~! (Efun ([mk_binder "n"], None, mk_Pvar "n", MaskVisible,
 let example4 : expr = ~! (Esequence (
   mk_assume (ge (mk_Tvar "n") (~@ (Tconst (int_const_of_int 0)))),
   mk_assert (ge (mk_Tvar "n") (~@ (Tconst (int_const_of_int 0))))))
+
+(* ----- example 5 -----
+    fn is_prime(n: u64) -> bool {
+      if n < 2 { return false; }
+      if n == 2 { return true; }
+      if n % 2 == 0 { return false; }
+
+      let m: u64 = n - 1;
+      let mut i: u64 = 3;
+
+      loop {
+        if i > m { return true; }
+        if n % i == 0 { return false; }
+        i = i + 2;
+      }
+    }
+
+    fn prime_spec() forall {
+      let n: u64 = @;
+
+      assume { assert(n > 1); }
+
+      if is_prime(n) {
+        let m: u64 = @;
+        assume { assert(m > 1 && m < n); }
+        assert(n % m > 0);
+      }
+
+      else exists {
+        let m: u64 = @;
+        assert(m > 1 && m < n);
+        assert(n % m == 0);
+      }
+    } *)
